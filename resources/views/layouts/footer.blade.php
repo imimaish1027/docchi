dbc98a523be33c627b3a0be6e8250ea153e9e179
tarosukey <show10turn@gmail.com>
2021-01-02 11:13:08 +0900

    フッターを作成

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,11 @@
+<footer class="l-footer">
+  <div class="p-footer__container">
+    <div class="p-footer__head">
+      <div class="p-footer__nav">
+        <a class="c-footer__nav__one">TOP</a>
+        <a class="c-footer__nav__one">お問い合わせ</a>
+      </div>
+    </div>
+    <div class="p-footer__foot">copyright ©️ 2021 Choice all rights reserved.</div>
+  </div>
+</footer>
