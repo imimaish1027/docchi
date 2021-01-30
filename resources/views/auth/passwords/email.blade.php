1e06ae9dcefe3f39bcd7ccb749a2a4a1daa6ce51
tarosukey <show10turn@gmail.com>
2021-01-30 21:47:12 +0900

    パスワードリマインダー能を追加(#15)

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,47 +1,35 @@
 @extends('layouts.app')
 
+@section('title', 'パスワードリセット')
+
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    <form method="POST" action="{{ route('password.email') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
+<div class="p-form">
+    <div class="p-form__title">
+        <p class="c-form__title__text">パスワードリセット</p>
     </div>
+
+    <form method="POST" action="{{ route('password.email') }}" class="p-form__main">
+        @csrf
+        <p class="c-form__note">パスワードを再設定するためのリンクを送ります。</p>
+
+        <div class="p-form__one p-form__text">
+            <p class="c-form__one__title">メールアドレス</p>
+            <input id="email" type="text" name="email" value="{{ old('email') }}" autocomplete="email" class="c-form__text form-control @error('email') is-invalid @enderror">
+        </div>
+        @error('email')
+        <div class="invalid__feedback" role="alert">
+            <strong>{{ $message }}</strong>
+        </div>
+        @enderror
+
+        <div class="c-btn__area">
+            <button type="submit" class="c-form__btn btn">
+                {{ __('送信') }}
+            </button>
+        </div>
+
+    </form>
 </div>
+
 @endsection
