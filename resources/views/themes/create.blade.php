ac2955e909459d299a22f0b5923a60c869c9e838
tarosukey <show10turn@gmail.com>
2021-01-31 17:50:18 +0900

    フォームのスタイルを修正

--- resources/views/themes/create.blade.php
+++ resources/views/themes/create.blade.php
@@ -12,7 +12,9 @@
     @csrf
     <div class="p-form__one p-form__text">
       <p class="c-form__one__title">テーマ名</p>
-      <input id="title" type="text" name="title" value="{{ old('title') }}" autocomplete="title" class="c-form__text form-control @error('title') is-invalid @enderror">
+      <div class="p-form__input">
+        <input id="title" type="text" name="title" value="{{ old('title') }}" autocomplete="title" class="c-form__text form-control @error('title') is-invalid @enderror">
+      </div>
     </div>
     @error('title')
     <div class="invalid__feedback" role="alert">
@@ -22,7 +24,9 @@
 
     <div class="p-form__one p-form__text">
       <p class="c-form__one__title">タグ</p>
-      <input id="tag" type="text" name="tag" value="{{ old('tag') }}" autocomplete="tag" class="c-form__text form-control @error('tag') is-invalid @enderror">
+      <div class="p-form__input">
+        <input id="tag" type="text" name="tag" value="{{ old('tag') }}" autocomplete="tag" class="c-form__text form-control @error('tag') is-invalid @enderror">
+      </div>
     </div>
     @error('tag')
     <div class="invalid__feedback" role="alert">
@@ -32,7 +36,9 @@
 
     <div class="p-form__one p-form__text">
       <p class="c-form__one__title">選択肢Aの回答</p>
-      <input id="answer" type="text" name="answer_a" value="{{ old('answer_a') }}" autocomplete="answer_a" class="c-form__text form-control @error('answer_a') is-invalid @enderror">
+      <div class="p-form__input">
+        <input id="answer" type="text" name="answer_a" value="{{ old('answer_a') }}" autocomplete="answer_a" class="c-form__text form-control @error('answer_a') is-invalid @enderror">
+      </div>
     </div>
     @error('answer_a')
     <div class="invalid__feedback" role="alert">
@@ -42,11 +48,13 @@
 
     <div class="p-form__one p-form__pic">
       <p class="c-form__one__title">選択肢Aの画像</p>
-      <label class="p-form__pic__one p-area__drop  form-control @error('pic_a') is-invalid @enderror">
-        <input type="hidden" name="MAX_FILE_SIZE" value="3145728">
-        <input id="pic" type="file" name="pic_a" value="{{ old('pic_a') }}" class="c-form__pic">
-        <p class="c-area__drop__text">クリックorドラッグ&ドロップで<br>画像をアップロード</p>
-      </label>
+      <div class="p-form__input">
+        <label class="p-form__pic__one p-area__drop  form-control @error('pic_a') is-invalid @enderror">
+          <input type="hidden" name="MAX_FILE_SIZE" value="3145728">
+          <input id="pic" type="file" name="pic_a" value="{{ old('pic_a') }}" class="c-form__pic">
+          <p class="c-area__drop__text">クリックorドラッグ&ドロップで<br>画像をアップロード</p>
+        </label>
+      </div>
     </div>
     @error('pic_a')
     <div class="invalid__feedback c-error__pic__text" role="alert">
@@ -56,7 +64,9 @@
 
     <div class="p-form__one p-form__text">
       <p class="c-form__one__title">選択肢Bの回答</p>
-      <input id="answer" type="text" name="answer_b" value="{{ old('answer_b') }}" autocomplete="answer_b" class="c-form__text form-control @error('answer_b') is-invalid @enderror">
+      <div class="p-form__input">
+        <input id="answer" type="text" name="answer_b" value="{{ old('answer_b') }}" autocomplete="answer_b" class="c-form__text form-control @error('answer_b') is-invalid @enderror">
+      </div>
     </div>
     @error('answer_b')
     <div class="invalid__feedback" role="alert">
@@ -66,11 +76,13 @@
 
     <div class="p-form__one p-form__pic">
       <p class="c-form__one__title">選択肢Bの画像</p>
-      <label class="p-form__pic__one p-area__drop  form-control @error('pic_b') is-invalid @enderror">
-        <input type="hidden" name="MAX_FILE_SIZE" value="3145728">
-        <input id="pic" type="file" name="pic_b" value="{{ old('pic_b') }}" class="c-form__pic">
-        <p class="c-area__drop__text">クリックorドラッグ&ドロップで<br>画像をアップロード</p>
-      </label>
+      <div class="p-form__input">
+        <label class="p-form__pic__one p-area__drop  form-control @error('pic_b') is-invalid @enderror">
+          <input type="hidden" name="MAX_FILE_SIZE" value="3145728">
+          <input id="pic" type="file" name="pic_b" value="{{ old('pic_b') }}" class="c-form__pic">
+          <p class="c-area__drop__text">クリックorドラッグ&ドロップで<br>画像をアップロード</p>
+        </label>
+      </div>
     </div>
     @error('pic_b')
     <div class="invalid__feedback c-error__pic__text" role="alert">
